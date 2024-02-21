537422e33d8b571d8a961b8b96af3214447fb800
Suyeon <msjgr2801@gmail.com>
2024-02-21 15:48:48 +0900

    build: 필요한 모듈 및 환경 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,52 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:prettier/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+	plugins: ['react-refresh', 'import', '@emotion'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: { tsconfigRootDir: __dirname },
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'auto',
+			},
+		],
+		'import/order': [
+			'error',
+			{
+				groups: [
+					'type',
+					'builtin',
+					'external',
+					'internal',
+					'parent',
+					'sibling',
+					'index',
+					'unknown',
+				],
+				pathGroups: [
+					{
+						pattern: '~/*',
+						group: 'internal',
+						position: 'after',
+					},
+				],
+				alphabetize: { order: 'asc', caseInsensitive: true },
+			},
+		],
+	},
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+	return defineConfig({
+		plugins: [react(), svgr()],
+		resolve: {
+			alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
+		},
+	});
+};
