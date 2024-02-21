d9a85fd8762c54a5f15b2de7649d15fa92730e7c
Suyeon <msjgr2801@gmail.com>
2024-02-21 16:01:41 +0900

    chore: 린터 및 프리티어 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default ({ mode }) => {
 	return defineConfig({
 		plugins: [react(), svgr()],
 		resolve: {
-			alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
-		},
+			alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }]
+		}
 	});
 };
