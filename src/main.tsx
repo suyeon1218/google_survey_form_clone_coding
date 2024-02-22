ed00a1861da88f398ccacf5d542e0b51a1b55b73
Suyeon <msjgr2801@gmail.com>
2024-02-22 13:22:05 +0900

    feat: 차크라 Theme 타입 추가하기

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import { theme } from './styles/theme';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<ChakraProvider theme={theme}>
+			<App />
+		</ChakraProvider>
 	</StrictMode>
 );
