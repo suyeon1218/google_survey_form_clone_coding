9e13178836e0d82e87dc95153a90bbf2ce13a9ac
Suyeon <msjgr2801@gmail.com>
2024-04-22 20:24:09 +0900

    feat: 분리한 Input 컴포넌트 연결하기

--- src/components/ResponseCard/index.tsx
+++ src/components/ResponseCard/index.tsx
@@ -1,7 +1,9 @@
 import { memo } from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
+import InputCheckbox from '../InputCheckbox';
+import InputDropDown from '../InputDropDown';
 import InputLong from '../InputLong';
-import InputOptions from '../InputOptions';
+import InputRadio from '../InputRadio';
 import InputShort from '../InputShort';
 import { CardType, RootStateType } from './../../store/index';
 import * as S from './index.style';
@@ -34,9 +36,9 @@ const ResponseCard = memo(({ id }: ResponseCardProps) => {
 				{type === 'title' && <InputLong id={id} />}
 				{type === 'short' && <InputShort id={id} />}
 				{type === 'long' && <InputLong id={id} />}
-				{(type === 'radio' || type === 'checkbox' || type === 'dropdown') && (
-					<InputOptions id={id} />
-				)}
+				{type === 'radio' && <InputRadio id={id} />}
+				{type === 'checkbox' && <InputCheckbox id={id} />}
+				{type === 'dropdown' && <InputDropDown id={id} />}
 			</S.Body>
 		</S.Container>
 	);
