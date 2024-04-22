015d2e0c2aa2c350faece4ae03bc6b9451a9235b
Suyeon <msjgr2801@gmail.com>
2024-04-22 20:23:36 +0900

    feat: InputOptions 의 드롭다운 타입을InputDropDown 으로 분리

---
+++ src/components/InputDropDown/index.tsx
@@ -0,0 +1,37 @@
+import { Stack } from '@chakra-ui/react';
+import { Controller, useFormContext } from 'react-hook-form';
+import { useSelector, shallowEqual } from 'react-redux';
+import { CardType, RootStateType } from '~/store';
+import DropDown from '../DropDown';
+
+interface InputDropDownProps {
+	id: string;
+}
+
+const InputDropDown = ({ id }: InputDropDownProps) => {
+	const { control } = useFormContext();
+	const { options } = useSelector((state: RootStateType) => {
+		const targetCard = state.cards.find((card) => card.id === id) as CardType;
+
+		return { options: targetCard.options };
+	}, shallowEqual);
+
+	return (
+		<Stack direction={'column'}>
+			<Controller
+				name={id}
+				control={control}
+				render={({ field: { onChange, value } }) => (
+					<DropDown
+						menuList={options.map((option) => option.content)}
+						includeDefaultValue={true}
+						defaultValue={value}
+						onClick={onChange}
+					/>
+				)}
+			/>
+		</Stack>
+	);
+};
+
+export default InputDropDown;
